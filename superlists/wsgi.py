205ea82291112faf06fda8f617b33056d677aecc
Hannah Balenda <hviolet@thestranger.com>
2018-07-24 11:29:39 -0700

    Add dotenv

--- superlists/wsgi.py
+++ superlists/wsgi.py
@@ -9,7 +9,13 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
+from dotenv import load_dotenv
+
 from django.core.wsgi import get_wsgi_application
+
+project_folder = os.path.expanduser('~/testing-goat')
+
+load_dotenv(os.path.join(project_folder, '.env'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
 
